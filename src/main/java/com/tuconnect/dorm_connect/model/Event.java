606bd0cbcec57b0798d5996ad8a00ff1391a7538
Milena Angelova <164063109+MillaA2004@users.noreply.github.com>
2025-11-16 00:27:31 +0200

    entity for Event

---
+++ src/main/java/com/tuconnect/dorm_connect/model/Event.java
@@ -0,0 +1,37 @@
+package com.tuconnect.dorm_connect.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "event")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "organizer_id")
+    private User organizer;
+
+    @ManyToMany
+    @JoinTable(
+            name = "events_participants",
+            joinColumns = @JoinColumn(name = "event_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> participants;
+}
