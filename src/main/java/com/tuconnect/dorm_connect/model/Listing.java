2c8a585ea1845ca1e640ee0ca77e763378206a60
Milena Angelova <164063109+MillaA2004@users.noreply.github.com>
2025-11-16 00:24:04 +0200

    entity for Listing

---
+++ src/main/java/com/tuconnect/dorm_connect/model/Listing.java
@@ -0,0 +1,28 @@
+package com.tuconnect.dorm_connect.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "listing")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Listing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
