7e357663a933cd3285dbc69629dece8393afc57e
Milena Angelova <164063109+MillaA2004@users.noreply.github.com>
2025-11-16 00:13:25 +0200

    entity for User

---
+++ src/main/java/com/tuconnect/dorm_connect/model/User.java
@@ -0,0 +1,39 @@
+package com.tuconnect.dorm_connect.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    private String interests;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    // Relationships
+    @OneToMany(mappedBy = "user")
+    private List<Listing> listings;
+
+    @OneToMany(mappedBy = "user")
+    private List<Review> reviews;
+
+    @ManyToMany(mappedBy = "participants")
+    private List<Event> events;
+}
