3e5e9dff6683c7d980ba52876446611c90abc750
Milena Angelova <164063109+MillaA2004@users.noreply.github.com>
2025-11-20 19:35:47 +0200

    update on Listing Entity and ListingDTOs

--- src/main/java/com/tuconnect/dorm_connect/dto/Listing/ListingResponseDTO.java
+++ src/main/java/com/tuconnect/dorm_connect/dto/Listing/ListingResponseDTO.java
@@ -1,13 +1,19 @@
 package com.tuconnect.dorm_connect.dto.Listing;
 
+import java.time.LocalDateTime;
+
 public record ListingResponseDTO(
         Long id,
         String title,
         String description,
         Double price,
+        String preferencesJson,
         Long dormId,
         String dormName,
         Long userId,
-        String username
+        String username,
+        Boolean isActive,
+        LocalDateTime createdAt,
+        LocalDateTime expiresAt
 ) {
 }
