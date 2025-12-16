907bcbd204a821d65ca112cfa6e42387fcc72147
Martin Kanev <martin_kk@abv.bg>
2025-12-16 18:30:26 +0200

    Removed again useless comments

--- src/main/java/com/tuconnect/dorm_connect/service/implementations/MessageServiceImpl.java
+++ src/main/java/com/tuconnect/dorm_connect/service/implementations/MessageServiceImpl.java
@@ -18,69 +18,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 
-//@Service
-//public class MessageServiceImpl implements MessageService {
-//
-//    private final MessageRepository messageRepository;
-//    private final ChatRepository chatRepository;
-//    private final UserRepository userRepository;
-//    private final ChatService chatService;
-//    private final MessageMapper messageMapper;
-//
-//    @Autowired
-//    public MessageServiceImpl(MessageRepository messageRepository,ChatRepository chatRepository,UserRepository userRepository,ChatService chatService,MessageMapper messageMapper) {
-//        this.chatRepository=chatRepository;
-//        this.messageRepository = messageRepository;
-//        this.messageMapper=messageMapper;
-//        this.chatService=chatService;
-//        this.userRepository=userRepository;
-//    }
-//
-//
-//    public MessageDTO sendMessage(Long chatId, Long senderId, String content) {
-//        if (content == null || content.isBlank()) {
-//            throw new IllegalArgumentException("Message content cannot be empty");
-//        }
-//
-//
-//        chatService.assertUserInChat(senderId, chatId);
-//
-//
-//        Chat chat = chatRepository.findById(chatId)
-//                .orElseThrow(() -> new EntityNotFoundException("Chat not found: " + chatId));
-//
-//        User sender = userRepository.findById(senderId)
-//                .orElseThrow(() -> new EntityNotFoundException("User not found: " + senderId));
-//
-//
-//        Message message = new Message();
-//        message.setChat(chat);
-//        message.setSender(sender);
-//        message.setContent(content);
-//        message.setSentAt(Instant.now());
-//
-//        Message saved = messageRepository.save(message);
-//
-//
-//        return messageMapper.toDto(saved);
-//    }
-//
-//
-//
-//    public Page<MessageDTO> getMessages(Long chatId, Long requesterId, int page, int size) {
-//
-//        chatService.assertUserInChat(requesterId, chatId);
-//
-//
-//        PageRequest pageable = PageRequest.of(page, size);
-//        Page<Message> messagesPage =
-//                messageRepository.findByChatChatIdOrderBySentAtAsc(chatId, pageable);
-//
-//
-//        return messagesPage.map(messageMapper::toDto);
-//    }
-//}
-
 
 
 @Service
