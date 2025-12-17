b84c7cf2b47b98fda9763c47a7b139837c0db083
Milena Angelova <164063109+MillaA2004@users.noreply.github.com>
2025-12-17 21:12:44 +0200

    Questionnaire elements

---
+++ src/main/java/com/tuconnect/dorm_connect/controller/QuestionnaireController.java
@@ -0,0 +1,29 @@
+package com.tuconnect.dorm_connect.controller;
+
+import com.tuconnect.dorm_connect.dto.Questionnaire.QuestionnaireDTO;
+import com.tuconnect.dorm_connect.service.QuestionnaireService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/questionnaires")
+@RequiredArgsConstructor
+public class QuestionnaireController {
+
+    private final QuestionnaireService questionnaireService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<QuestionnaireDTO> createOrUpdate(
+            @PathVariable Long userId,
+            @RequestBody QuestionnaireDTO questionnaireDTO) {
+        QuestionnaireDTO saved = questionnaireService.saveForUser(userId, questionnaireDTO);
+        return ResponseEntity.ok(saved);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<QuestionnaireDTO> getByUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(questionnaireService.getByUser(userId));
+    }
+}
