272aeec96dc60246fdd07c0f31262dba08f44c72
Milena Angelova <164063109+MillaA2004@users.noreply.github.com>
2026-01-07 12:06:01 +0200

    Created DormSeeder for the dorms and their information

---
+++ src/main/java/com/tuconnect/dorm_connect/startup/DataSeeder.java
@@ -0,0 +1,61 @@
+package com.tuconnect.dorm_connect.startup;
+
+import com.tuconnect.dorm_connect.model.Dorm;
+import com.tuconnect.dorm_connect.repository.DormRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Profile("dev")
+@Component
+@RequiredArgsConstructor
+@Order(0)
+public class DataSeeder implements CommandLineRunner {
+
+    private final DormRepository dormRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedDorms();
+    }
+
+    private void seedDorms() {
+        if (dormRepository.count() == 0) {
+            List<Dorm> dorms = List.of(
+                    Dorm.builder()
+                            .name("Block 54")
+                            .description("Family dorm.")
+                            .price(76.0)
+                            .address("Studentski Grad, Block 54")
+                            .latitude(42.6535)
+                            .longitude(23.3552)
+                            .build(),
+
+                    Dorm.builder()
+                            .name("Block 14")
+                            .description("Renewed dorm of TU from 2015 with solar panels.")
+                            .price(69.0)
+                            .address("Studentski Grad, Block 14")
+                            .latitude(42.6550)
+                            .longitude(23.3600)
+                            .build(),
+
+                    Dorm.builder()
+                            .name("BLock 3")
+                            .description("TU dorm for the 6.00 GPA")
+                            .price(96.0)
+                            .address("Studentski Grad, Block 3")
+                            .latitude(42.6510)
+                            .longitude(23.3510)
+                            .build()
+            );
+
+            dormRepository.saveAll(dorms);
+            System.out.println("✅ Database seeded with 3 Dorms.");
+        }
+    }
+}
